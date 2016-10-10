9fcb95060a1b5ab04b7a71c15dc2ce4f5f346866
manda.sun <manda.sun@qunar.com>
2016-10-10 23:23:17 +0800

    新增：新增工资model及数据表创建语句。

---
+++ src/main/java/com/springapp/mvc/Model/Salary.java
@@ -0,0 +1,148 @@
+package com.springapp.mvc.Model;
+
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2016/10/10.
+ */
+@Repository
+public class Salary {
+    private String owner;
+    private Date date;
+    private BigDecimal beforeTax;
+    private BigDecimal afterTax;
+    private BigDecimal houseFundsCompany;
+    private BigDecimal houseFunds;
+    private BigDecimal medicareCompany;
+    private BigDecimal medicare;
+    private BigDecimal pensionInsuranceCompany;
+    private BigDecimal pensionInsurance;
+    private BigDecimal unemployInsurance;
+    private BigDecimal unemployInsuranceCompany;
+    private BigDecimal tax;
+    private BigDecimal insuranceBase;
+    private BigDecimal fundBase;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public BigDecimal getUnemployInsuranceCompany() {
+        return unemployInsuranceCompany;
+    }
+
+    public void setUnemployInsuranceCompany(BigDecimal unemployInsuranceCompany) {
+        this.unemployInsuranceCompany = unemployInsuranceCompany;
+    }
+
+    public BigDecimal getInsuranceBase() {
+        return insuranceBase;
+    }
+
+    public void setInsuranceBase(BigDecimal insuranceBase) {
+        this.insuranceBase = insuranceBase;
+    }
+
+    public BigDecimal getFundBase() {
+        return fundBase;
+    }
+
+    public void setFundBase(BigDecimal fundBase) {
+        this.fundBase = fundBase;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public BigDecimal getBeforeTax() {
+        return beforeTax;
+    }
+
+    public void setBeforeTax(BigDecimal beforeTax) {
+        this.beforeTax = beforeTax;
+    }
+
+    public BigDecimal getAfterTax() {
+        return afterTax;
+    }
+
+    public void setAfterTax(BigDecimal afterTax) {
+        this.afterTax = afterTax;
+    }
+
+    public BigDecimal getHouseFundsCompany() {
+        return houseFundsCompany;
+    }
+
+    public void setHouseFundsCompany(BigDecimal houseFundsCompany) {
+        this.houseFundsCompany = houseFundsCompany;
+    }
+
+    public BigDecimal getHouseFunds() {
+        return houseFunds;
+    }
+
+    public void setHouseFunds(BigDecimal houseFunds) {
+        this.houseFunds = houseFunds;
+    }
+
+    public BigDecimal getMedicareCompany() {
+        return medicareCompany;
+    }
+
+    public void setMedicareCompany(BigDecimal medicareCompany) {
+        this.medicareCompany = medicareCompany;
+    }
+
+    public BigDecimal getMedicare() {
+        return medicare;
+    }
+
+    public void setMedicare(BigDecimal medicare) {
+        this.medicare = medicare;
+    }
+
+    public BigDecimal getTax() {
+        return tax;
+    }
+
+    public void setTax(BigDecimal tax) {
+        this.tax = tax;
+    }
+
+    public BigDecimal getPensionInsurance() {
+        return pensionInsurance;
+    }
+
+    public void setPensionInsurance(BigDecimal pensionInsurance) {
+        this.pensionInsurance = pensionInsurance;
+    }
+
+    public BigDecimal getPensionInsuranceCompany() {
+        return pensionInsuranceCompany;
+    }
+
+    public void setPensionInsuranceCompany(BigDecimal pensionInsuranceCompany) {
+        this.pensionInsuranceCompany = pensionInsuranceCompany;
+    }
+
+    public BigDecimal getUnemployInsurance() {
+        return unemployInsurance;
+    }
+
+    public void setUnemployInsurance(BigDecimal unemployInsurance) {
+        this.unemployInsurance = unemployInsurance;
+    }
+}
