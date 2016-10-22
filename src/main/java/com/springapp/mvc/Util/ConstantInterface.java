7fb3e1222d2b58a1fcdeb2f60e5ec9fdd769e3a7
manda.sun <manda.sun@qunar.com>
2016-10-22 17:48:14 +0800

    优化：修改每月消费预算5000->700

--- src/main/java/com/springapp/mvc/Util/ConstantInterface.java
+++ src/main/java/com/springapp/mvc/Util/ConstantInterface.java
@@ -6,5 +6,5 @@ import java.math.BigDecimal;
  * Created by Administrator on 2016/10/16.
  */
 public interface ConstantInterface {
-    BigDecimal Budget = new BigDecimal(5000);
+    BigDecimal Budget = new BigDecimal(7000);
 }
